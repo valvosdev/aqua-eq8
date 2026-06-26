c5b2391fbce89ebd3e9bda5b5f01888f5ec4929c
Ion Suruceanu <ion.suruceanu@quarta.energy>
2026-06-26 16:51:35 +0100

    added certs and use them to communicate via a secure line

--- deployment/README.md
+++ deployment/README.md
@@ -5,4 +5,39 @@
 
 ## MQTT
 
+Run ``` ./generate-cert-key.sh ``` first to generate self signed certificates 
+
+Create secrets for certs files
+
+```
+kubectl create secret generic mqtt-tls-certs \
+  --from-file=valvos-dev-ca.crt=./certs/valvos-dev-ca.crt \
+  --from-file=server-crt.crt=./certs/server.crt \
+  --from-file=server-crt.key=./certs/server.key \
+  -n valvos
+```
+
 ``` helm upgrade -i mqtt t3n/mosquitto --namespace valvos  -f ./mqtt/values.yaml ```
+
+### Test connectivity:
+
+Replace the IP address with yours
+
+```
+mosquitto_sub -h 10.30.10.30 -p 8883 -t "test/topic" \
+  --cafile ./certs/valvos-dev-ca.crt \
+  --cert ./certs/client.crt \
+  --key ./certs/client.key \
+  --insecure -v
+```
+
+Publish a message
+
+```
+ mosquitto_pub -h 10.30.10.30 -p 8883 -t "test/topic" \
+  -m "Hello from macOS over MQTTS" \
+  --cafile ./certs/valvos-dev-ca.crt \
+  --cert ./certs/client.crt \
+  --key ./certs/client.key \
+  --insecure
+  ```
--- deployment/generate-cert-key.sh
+++ deployment/generate-cert-key.sh
@@ -7,19 +7,19 @@ SUBJECT_CLIENT="/C=SE/ST=Bristol/L=Bristol/O=valvos/OU=Client/CN=$IP"
 
 function generate_CA () {
    echo "$SUBJECT_CA"
-   openssl req -x509 -nodes -sha256 -newkey rsa:2048 -subj "$SUBJECT_CA"  -days 365 -keyout ./certs/ca.key -out ./certs/ca.crt
+   openssl req -x509 -nodes -sha256 -newkey rsa:2048 -subj "$SUBJECT_CA"  -days 365 -keyout ./certs/valvos-dev-ca.key -out ./certs/valvos-dev-ca.crt
 }
 
 function generate_server () {
    echo "$SUBJECT_SERVER"
    openssl req -nodes -sha256 -new -subj "$SUBJECT_SERVER" -keyout ./certs/server.key -out ./certs/server.csr
-   openssl x509 -req -sha256 -in ./certs/server.csr -CA ./certs/ca.crt -CAkey ./certs/ca.key -CAcreateserial -out ./certs/server.crt -days 365
+   openssl x509 -req -sha256 -in ./certs/server.csr -CA ./certs/valvos-dev-ca.crt -CAkey ./certs/valvos-dev-ca.key -CAcreateserial -out ./certs/server.crt -days 365
 }
 
 function generate_client () {
    echo "$SUBJECT_CLIENT"
    openssl req -new -nodes -sha256 -subj "$SUBJECT_CLIENT" -out ./certs/client.csr -keyout ./certs/client.key 
-   openssl x509 -req -sha256 -in ./certs/client.csr -CA ./certs/ca.crt -CAkey ./certs/ca.key -CAcreateserial -out ./certs/client.crt -days 365
+   openssl x509 -req -sha256 -in ./certs/client.csr -CA ./certs/valvos-dev-ca.crt -CAkey ./certs/valvos-dev-ca.key -CAcreateserial -out ./certs/client.crt -days 365
 }
 
 generate_CA
